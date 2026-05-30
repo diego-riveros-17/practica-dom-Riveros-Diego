6df830e2eff2b104e6b22df2447786ac6b843aa4
unknown <diegoriveros1991@gmail.com>
2026-05-29 22:26:04 -0300

    feat: se cambio la funcion autoinvocada por una funcion de expresion, para poder llamarla cuando se cargue, elimine o se busque un superheroe

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,27 +38,43 @@ const personajes = [
 
 const contenedor = document.querySelector("#cardSuperheroe");
 
-// console.log(Superheroe);
+// console.log(contenedor);
 
 // const cargarSuperheroes = contenedor.addEventListener("click", () => {
 //   //   console.log("click");
 
 // });
 
-(() => {
+const cargarSuperheroes = () => {
   personajes.forEach((personaje) => {
     // console.log(personaje);
     contenedor.innerHTML += `<div class="col-4 my-2 d-flex justify-content-center" data-id="${personaje.id}">
-            <div class="card" >
-              <img src="${personaje.imagen}" class="card-img-top" alt="..." style="height: 20rem; object-fit: contain"/>
+            <div class="card" style="width: 25rem">
+              <img src="${personaje.imagen}" class="card-img-top" alt="..." style="height: 25rem;"/>
               <div class="card-body">
                 <h5 class="card-title">${personaje.nombre}</h5>
               </div>
 
               <div class="card-body">
-                <button class="btn btn-danger">Eliminar</button>
+                <button class="btn btn-danger btnEliminar">Eliminar</button>
               </div>
             </div>
           </div>`;
   });
-})();
+};
+
+cargarSuperheroes();
+
+contenedor.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btnEliminar")) {
+    const cardSuperheroe = e.target.closest(".col-4");
+    const idPersonaje = Number(cardSuperheroe.dataset.id);
+    //console.log(idPersonaje);
+
+    cardSuperheroe.remove();
+  }
+});
+
+// btnEliminar.addEventListener("click", () => {
+//   const idPersonaje = btnEliminar.closest(".col-4").value;
+// });
